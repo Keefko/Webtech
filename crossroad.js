988bdac321506afc848c8d46a484155ed3823bb6
Dan Kohout <kohooutdan@gmail.com>
2020-01-03 21:11:04 +0100

    prva krizovatka almost done

--- crossroad.js
+++ crossroad.js
@@ -1,13 +1,11 @@
 var canvas = document.getElementById("canvas");
-var canvas2 = document.getElementById("canvas-2");
 canvas.width = 1000;
 canvas.height = 500;
 var context = canvas.getContext("2d");
 var isRedActive = false; isBlueActive = false; isGreenActive = false;
 var isBlueReady = false, isRedReady = false, isGreenReady = true;
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
 var road = new Image();
 road.onload = function(){
@@ -27,21 +25,21 @@ mainSign.src = "img/main.png";
 
 var greenCarSprites = new Image();
 greenCarSprites.onload = function(){
-	carGreen = new car(310,190,102,53);
+	carGreen = new car(310,187,102,53);
 	isGreenActive = true;
 }
 greenCarSprites.src = "img/greencarsprites.png"; 
 
 var blueCarSprites = new Image();
 blueCarSprites.onload = function(){
-	carBlue = new car(540,240,44,108);
+	carBlue = new car(500,250,108,108);
 	isBlueActive = true;
 }
 blueCarSprites.src = "img/bluecarsprites.png";
 
 var redCarSprites = new Image();
 redCarSprites.onload = function(){
-	carRed = new car(630,110,102,98);
+	carRed = new car(630,100,98,98);
 	isRedActive = true;
 }
 redCarSprites.src = "img/redcarsprites.png";
@@ -82,7 +80,7 @@ function getMousePosition(e){
 function draw(){	
 	context.drawImage(road,0,100,1000,400);
 
-	if(isRedActive){
+	if(isRedActive == true){
 		context.drawImage(redCarSprites,0,0,carRed.width,carRed.height,carRed.x,carRed.y,carRed.width,carRed.height);
 	}
 
@@ -100,38 +98,67 @@ function draw(){
 }
 
 
-
-
 let frameCountGreen = 0; 
-let count = 0;
+
+var fpsC = 22; 
+var nowC,deltaC;
+var thenC = Date.now();
+var intervalC = 1000/fpsC;
 
 function greenCarMove(){
-	frameCountGreen++;
-	if(frameCountGreen > 15){
-		window.requestAnimationFrame(greenCarMove);
-		return; 
-	}
-	frameCountGreen = 0; 
-	context.clearRect(0,0,canvas.width,canvas.height);
-	draw();
-	context.drawImage(greenCarSprites,count*carGreen.width,0,carGreen.width,carGreen.height,carGreen.x,carGreen.y,carGreen.width,carGreen.height);
-	carGreen.newPos(3,0);
 
-	window.requestAnimationFrame(greenCarMove);
-}
-
-
-//na kokot 
-function blueCarMove(){ 
-	var number = 0; 
-	var interval = setInterval(function(){
+	requestAnimationFrame(greenCarMove);
+	nowC = Date.now();
+	deltaC = nowC - thenC;
+	if(deltaC > intervalC){
 		context.clearRect(0,0,canvas.width,canvas.height);
 		draw();
-		number++;
-		number = number % carBlue.frame;
-		context.drawImage(blueCarSprites,number*carBlue.width,0,carBlue.width,carBlue.height,carBlue.x,carBlue.y,carBlue.width,carBlue.height);
-		carBlue.newPos(0,0);
-	},400);
+		context.drawImage(greenCarSprites,carGreen.width,0,carGreen.width,carGreen.height,carGreen.x,carGreen.y,carGreen.width,carGreen.height);
+		carGreen.newPos(3,0);
+
+		thenC = nowC - (deltaC % intervalC);
+	}
+}
+
+const cycleLoopBlue = [0,1,2,3,4,5];
+let currentLoopIndexBlue = 0; 
+let currentDirectionBlue = 0;
+
+
+var fpsB = 22; 
+var nowB,deltaB;
+var thenB = Date.now();
+var intervalB = 1000/fpsB;
+
+function blueCarMove(){ 
+	requestAnimationFrame(blueCarMove);
+	nowB = Date.now();
+	deltaB = nowB - thenB;
+	if(deltaB > intervalB){
+		context.clearRect(0,0,canvas.width,canvas.height);
+		draw();
+		context.drawImage(blueCarSprites,cycleLoopBlue[currentLoopIndexBlue] * carBlue.width,currentDirectionBlue*carBlue.height,carBlue.width,carBlue.height,carBlue.x,carBlue.y,carBlue.width,carBlue.height);
+		
+		if(currentDirectionBlue == 0){
+			carBlue.newPos(0,-5);
+		} else if(currentDirectionBlue == 1){
+			carBlue.newPos(1,-5);
+		} else if(currentDirectionBlue == 2){
+			carBlue.newPos(6,-3);
+		} else if(currentDirectionBlue == 3){
+			carBlue.newPos(6,-1);
+		} else if(currentDirectionBlue == 4 || currentDirectionBlue == 5){
+			carBlue.newPos(20,0);
+		}
+
+		currentLoopIndexBlue++;
+		if(currentLoopIndexBlue >= cycleLoopBlue.length){
+			currentLoopIndexBlue = 0;
+			currentDirectionBlue++; 
+
+		}
+		thenB = nowB - (deltaB % intervalB);
+	}
 }
 
 /*https://dev.to/martyhimmel/animating-sprite-sheets-with-javascript-ag3*/
@@ -139,38 +166,40 @@ function blueCarMove(){
 
 const cycleLoopRed = [0,1,2,3,4,5,6,7,8];
 let currentLoopIndexRed = 0; 
-let frameCountRed = 0; 
 let currentDirectionRed = 0; 
+var now,delta;
+var fps = 22; 
+var then = Date.now();
+var interval = 1000/fps;
 
 function redCarMove(){
-	frameCountRed++;
-	if(frameCountRed > 15){
-		window.requestAnimationFrame(redCarMove);
-		return;
-	}
-	frameCount = 0;
-	context.clearRect(0,0,canvas.width,canvas.height);
-	draw();
-	context.drawImage(redCarSprites,cycleLoopRed[currentLoopIndexRed] * carRed.width,currentDirectionRed * carRed.height,carRed.width,carRed.height,0,0,carRed.width,carRed.height);
-	if(currentDirectionRed == 0){
-		carRed.newPos(-3,0);
-	}else if(currentDirectionRed == 1){
-		carRed.newPos(-3,0);
-	}else if(currentDirectionRed == 2){
-		carRed.newPos(-1,3);
-	}else if(currentDirectionRed == 3){
-		carRed.newPos(0,3);
-	}else if(currentDirectionRed == 4){
-		carRed.newPos(0,3);
-	}
 
-	currentLoopIndexRed++;
-	if(currentLoopIndexRed >= cycleLoopRed.length){
-		currentLoopIndexRed = 0; 
-		currentDirectionRed++;
+	requestAnimationFrame(redCarMove);
+	now = Date.now();
+	delta = now - then;
+	if(delta > interval){
+		context.clearRect(0,0,canvas.width,canvas.height);
+		draw();
+		context.drawImage(redCarSprites,cycleLoopRed[currentLoopIndexRed] * carRed.width,currentDirectionRed * carRed.height,carRed.width,carRed.height,carRed.x,carRed.y,carRed.width,carRed.height);
+		if(currentDirectionRed == 0){
+			carRed.newPos(-10,0);
+		} else if(currentDirectionRed == 1){
+			carRed.newPos(-10,0);
+		} else if(currentDirectionRed == 2){
+			carRed.newPos(-4,5);
+		} else if(currentDirectionRed == 3){
+			carRed.newPos(0,15);
+		} else if(currentDirectionRed == 4){
+			carRed.newPos(0,15);
+		}
+		currentLoopIndexRed++;
+		if(currentLoopIndexRed >= cycleLoopRed.length){
+			currentLoopIndexRed = 0; 
+			currentDirectionRed++;
+		}
+		then = now - (delta%interval);
 	}
-	window.requestAnimationFrame(redCarMove);
-}
+}	
 
 
 
@@ -186,15 +215,15 @@ function update(type){
 	switch(type){
 		case "blue" : 
 			isBlueActive= false;
-			blueCarMove();
+			requestAnimationFrame(blueCarMove);
 			break;
 		case "red" : 
 			isRedActive = false;
-			window.requestAnimationFrame(redCarMove);
+			requestAnimationFrame(redCarMove);
 			break;
 		case "green" : 
 			isGreenActive = false;
-			window.requestAnimationFrame(greenCarMove);
+			requestAnimationFrame(greenCarMove);
 			break;
 	}
 }
@@ -213,3 +242,13 @@ $(document).ready(function(){
 		}
 	});
 });  
+
+
+
+
+
+function simulation(){
+
+	update("blue");
+	update("red");
+}
